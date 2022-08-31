00c9e83ba8f4a725f95197db47d788225c338f6d
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-08-31 11:37:06 +0100

    doc: add docstring for python haversine function

--- fast_haversine/slow.py
+++ fast_haversine/slow.py
@@ -2,7 +2,28 @@ import math
 
 
 def slow_haversine(lat1: float, lng1: float, lat2: float, lng2: float) -> float:
-    # From https://bit.ly/3wKKYo1
+    """A pure python implementation of Haversine distance
+
+    This function is implemented following the ArcGIS documentation at
+    https://bit.ly/3wKKYo1. It is provided here as a convenience to perform
+    benchmarks against the Rust implementation.
+
+    Parameters
+    ----------
+    lat1 : float
+        First point latitude
+    lng1 : float
+        First point longitude
+    lat2 : float
+        Second point latitude
+    lng2 : float
+        Second point longitude
+
+    Returns
+    -------
+    float
+        Haversine distance in metres
+    """
     R = 6371008.8  # radius of Earth in meters
     phi_1 = math.radians(lat1)
     phi_2 = math.radians(lat2)
