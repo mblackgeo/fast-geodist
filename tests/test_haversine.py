96754fce610f0ad979ba217e8cb85a5df73d263d
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-08-31 10:32:25 +0100

    test: implement test for numpy array

--- tests/test_haversine.py
+++ tests/test_haversine.py
@@ -1,6 +1,7 @@
+import numpy as np
 from pytest import approx
 
-from fast_haversine import haversine, haversine_vec, slow_haversine
+from fast_haversine import haversine, haversine_array, slow_haversine
 
 
 def test_fast_haversine():
@@ -10,7 +11,8 @@ def test_fast_haversine():
 
 
 def test_fast_haversine_vec():
-    res = haversine_vec([42.3521, 0.0], [-72.1235, 0.0], [70.612, 1.0], [72.1260, 0.0])
+    arr = np.array([[42.3521, -72.1235, 70.612, 72.1260], [0.0, 0.0, 1.0, 0.0]])
+    res = haversine_array(arr)
     expect = [7130580.307935911, 111195.0802335329]
     assert res[0] == approx(expect[0])
     assert res[1] == approx(expect[1])
