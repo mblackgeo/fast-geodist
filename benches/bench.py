1a38d578738b34e75563ac2890f8bf24f69ce81b
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-09-28 11:41:33 +0100

    refactor: simplify creation of array for benchmarking

--- benches/bench.py
+++ benches/bench.py
@@ -1,39 +1,23 @@
-import random
-
 import numpy as np
 
 from fast_geodist import haversine_array
 from fast_geodist.slow import slow_haversine
 
 
-def rand_lat() -> float:
-    return random.uniform(-90.0, 90.0)
-
-
-def rand_lng() -> float:
-    return random.uniform(-180.0, 180.0)
-
-
 def create_data() -> np.ndarray:
-    random.seed(0)
-    return np.array(
-        [(rand_lat(), rand_lng(), rand_lat(), rand_lng()) for _ in range(1_000_000)],
-    )
+    return np.random.uniform(low=-90.0, high=90.0, size=(1_000_000, 4))
 
 
-def bench_helper(data: np.ndarray, fast: bool = False):
-    if fast:
-        haversine_array(data)
-    else:
-        for row in data:
-            slow_haversine(*row)
+def bench_helper(data: np.ndarray):
+    for row in data:
+        slow_haversine(*row)
 
 
 def test_benchmark_fast(benchmark):
     data = create_data()
-    benchmark(bench_helper, data=data, fast=True)
+    benchmark(haversine_array, data)
 
 
 def test_benchmark_slow(benchmark):
     data = create_data()
-    benchmark(bench_helper, data=data, fast=False)
+    benchmark(bench_helper, data)
