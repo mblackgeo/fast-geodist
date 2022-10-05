5c0328acc3dde0a882775cafca33914b230e19c6
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2022-10-05 12:07:29 +0100

    feat: work in progress adding combinations function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use ndarray::parallel::prelude::{IndexedParallelIterator, IntoParallelIterator, ParallelIterator};
-use ndarray::{Array1, ArrayView2, Axis};
+use ndarray::{Array1, Array2, ArrayView2, Axis};
 use numpy::{IntoPyArray, PyArray1, PyReadonlyArray2};
 use pyo3::prelude::{pyfunction, pymodule, wrap_pyfunction, PyModule, PyResult, Python};
 
@@ -38,6 +38,14 @@ pub fn haversine_distance_array(x: &ArrayView2<f64>) -> Array1<f64> {
         .collect_into_vec(&mut distances);
 
     Array1::from(distances)
+}
+
+pub fn haversine_combinations(arr1: &ArrayView2<f64>, arr2: &ArrayView2<f64>) -> Array2<f64> {
+    // TODO panic if arr1 and arr2 aren't the same shape
+    let out = Array2::zeros((arr1.shape()[0], arr2.shape()[0]));
+
+    // TODO implement
+    out
 }
 
 /// Python wrappers
@@ -89,5 +97,20 @@ mod tests {
 
         assert_relative_eq!(res[0], expect[0], epsilon = 1.0e-6);
         assert_relative_eq!(res[1], expect[1], epsilon = 1.0e-6);
+    }
+
+    #[test]
+    fn test_haversine_comb() {
+        let arr1 = array![[45.7597, 4.8422], [51.509865, -0.118092]];
+        let arr2 = array![[48.8567, 2.3508], [40.7033962, -74.2351462]];
+
+        let res = haversine_combinations(&arr1.view(), &arr2.view());
+        let expect = array![[392217.25956, 343374.55271], [6163436.38211, 5586484.47423]];
+
+        for i in 0..expect.shape()[0] {
+            for j in 0..expect.shape()[1] {
+                assert_relative_eq!(res[(i, j)], expect[(i, j)], epsilon = 1.0e-6);
+            }
+        }
     }
 }
